ce7b707f716f8f138b768fbc5b771923bf46f7ba
huqiyang <qiyang.hqy@dtwave-inc.com>
2018-05-11 00:07:09 +0800

    增加：注册信息验证

--- sys/js/login.js
+++ sys/js/login.js
@@ -6,6 +6,7 @@ function init() {
   $('body').on('click', '.action-register', goRegisterPage);
   $('body').on('click', '.action-reg', goRegisterPage);
   $('body').on('click', '#regSellBtn', showRegSell);
+  $('body').on('click', '#reg-btn', doRegister);
 }
 
 function hideAllPages() {
@@ -35,4 +36,31 @@ function goRegisterPage() {
 function showRegSell() {
   hideAllPages();
   $('.regs').removeClass('hide');
+}
+
+function doRegister() {
+  const ids = ['mobile', 'qq', 'code', 'weixin', 'login-password', 'repassword'];
+  const infoMap = {};
+  const errMap = {};
+  ids.forEach(id => {
+    infoMap[id] = {
+      val: $(`#${id}`).val(),
+      isValid: true
+    };
+  });
+  for(let id in REGISTER_REGEXS) {
+    infoMap[id].isValid = REGISTER_REGEXS[id].test(infoMap[id].val);
+  }
+  // 确认密码
+  if (infoMap['repassword'].val !== infoMap['login-password'].val) {
+    infoMap['repassword'].isValid = false;
+  }
+  // 展示结果
+  ids.forEach(id => {
+    if (!infoMap[id].val || infoMap[id].val === '' || !infoMap[id].isValid) {
+      $(`#${id}`).siblings('p').find('span').removeClass('hide');
+    } else {
+      $(`#${id}`).siblings('p').find('span').addClass('hide');
+    }
+  })
 }
