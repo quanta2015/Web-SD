505534a0c78400a19c6cf2f3662eb30eebcc3293
quanta2006 <quanta2006@163.com>
2018-05-17 20:47:23 +0800

    添加买家管理页面mainBuy

---
+++ server/static/sys/js/mainsys.js
@@ -0,0 +1,30 @@
+$(init)
+
+function init() {
+  var h = $(document).height();
+  $("iframe").height(h - 50);
+
+  $('[data-button="bingShopBtn"]').on('click', function(e) {
+      $("#mainframe", parent.document.body).attr("src", "bindShop.html");
+  });
+  $('[data-button="listShopBtn"]').on('click', function(e) {
+      $("#mainframe", parent.document.body).attr("src", "listShop.html");
+  });
+
+  $('[data-button="createTaskBtn"]').on('click', function(e) {
+      $("#mainframe", parent.document.body).attr("src", "createTask.html");
+  });
+
+  $('[data-button="listTaskBtn"]').on('click', function(e) {
+      $("#mainframe", parent.document.body).attr("src", "listTask.html");
+  });
+
+  $('#exitBtn').on('click', function(e) {
+      location.href = 'index.html'
+  });
+
+
+}
+
+
+
