077226349cff87e0fa043d40a843b6bb470632e6
quanta2006 <quanta2006@163.com>
2018-05-17 20:58:20 +0800

    更新垫付任务界面

--- server/static/sys/js/mainsys.js
+++ server/static/sys/js/mainsys.js
@@ -4,23 +4,13 @@ function init() {
   var h = $(document).height();
   $("iframe").height(h - 50);
 
-  $('[data-button="bingShopBtn"]').on('click', function(e) {
-      $("#mainframe", parent.document.body).attr("src", "bindShop.html");
-  });
-  $('[data-button="listShopBtn"]').on('click', function(e) {
-      $("#mainframe", parent.document.body).attr("src", "listShop.html");
+  $('[data-button="auditTaskBtn"]').on('click', function(e) {
+      $("#mainframe", parent.document.body).attr("src", "auditTask.html");
   });
 
-  $('[data-button="createTaskBtn"]').on('click', function(e) {
-      $("#mainframe", parent.document.body).attr("src", "createTask.html");
-  });
-
-  $('[data-button="listTaskBtn"]').on('click', function(e) {
-      $("#mainframe", parent.document.body).attr("src", "listTask.html");
-  });
 
   $('#exitBtn').on('click', function(e) {
-      location.href = 'index.html'
+      location.href = 'auditTask.html'
   });
 
 
