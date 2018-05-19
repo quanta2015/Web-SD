35fecfc71e094c1934916dbf8d79b968c1e0015f
quanta2006 <quanta2006@163.com>
2018-05-19 14:37:35 +0800

    设置新手页面链接

--- server/static/sys/js/mainbuy.js
+++ server/static/sys/js/mainbuy.js
@@ -4,8 +4,8 @@ function init() {
   var h = $(document).height();
   $("iframe").height(h - 50);
 
-  $('[data-button="bingShopBtn"]').on('click', function(e) {
-      $("#mainframe", parent.document.body).attr("src", "bindShop.html");
+  $('[data-button="newTaskBtn"]').on('click', function(e) {
+      $("#mainframe", parent.document.body).attr("src", "newTask.html");
   });
  
   $('#exitBtn').on('click', function(e) {
