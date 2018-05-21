bec1cd67a8947267d478a536a82078878c71ee1e
huqiyang <qiyang.hqy@dtwave-inc.com>
2018-05-21 11:20:47 +0800

    优化绑定京东、淘宝

--- server/static/sys/js/bindTaobao.js
+++ server/static/sys/js/bindTaobao.js
@@ -3,8 +3,10 @@ $(init);
 function init() {
   initBindInfo();
 
+  
   $('body').on('click', '#returnBtn', doReturn );
   $('body').on('click', '#saveBtn', doSave);
+
 }
 
 async function initBindInfo() {
@@ -17,6 +19,12 @@ async function initBindInfo() {
     $('.container').append(await renderTmpl(TMPL_BIND_PLATFORM, {
       type: '淘宝',
       creditType: '花呗',
+      levels: [
+        '一星','二星','三星','四星','五星',
+        '一钻','二钻','三钻','四钻','五钻',
+        '一皇冠','二皇冠','三皇冠','四皇冠','五皇冠',
+        '一金冠','二金冠','三金冠','四金冠','五金冠',
+      ],
       list: [1,1,1],
       isbind: 0
     }));
@@ -25,14 +33,13 @@ async function initBindInfo() {
     console.log(cookie2('baitiaoImg', 'taobaoList'))
     $(".container").append(await renderTmpl(TMPL_BIND_PLATFORM, {
       platform: '淘宝',
+      creditType: '花呗',
       buyerId: cookie('id'),
       acount: cookie2('acount', 'taobaoList'),
       acountLevel: cookie2('acountLevel', 'taobaoList'),
       baitiaoStart: parseInt(cookie2('baitiaoStart', 'taobaoList'))?"checked":null,
       baitiaoUnStart: parseInt(cookie2('baitiaoStart', 'taobaoList'))?null:"checked",
-      receiveProvince:cookie2('acount', 'taobaoList'),
-      // receiveCity: cookie2('receiveCity', 'taobaoList'),
-      // receiveCountry: cookie2('receiveCountry', 'taobaoList'),
+      receiveProvince: cookie2('receiveProvince', 'taobaoList'),
       receiveAddress: cookie2('receiveAddress', 'taobaoList'),
       receiver: cookie2('receiver', 'taobaoList'),
       receiveMobile: cookie2('receiveMobile', 'taobaoList'),
@@ -45,6 +52,7 @@ async function initBindInfo() {
       type: "disabled"
     }) );
   }
+  $('#pick').distpicker();
 }
 
 
@@ -59,12 +67,10 @@ function doSave(data) {
     acount: $('#receiver').val(),
     acountLevel: $('#acount-level').val(),
     baitiaoStart: $("input[name='r-baitiao-start']:checked").val(),
-    receiveProvince: $('#receiver').val(),
-    // receiveCity: $('#receiver').val(),
-    // receiveCountry: $('#receiver').val(),
-    receiveAddress: $('#receiver').val(),
+    receiveProvince: $('#receive-province').val(),
+    receiveAddress: $('#receive-address').val(),
     receiver: $('#receiver').val(),
-    receiveMobile: $('#receiver').val(),
+    receiveMobile: $('#receive-mobile').val(),
     baitiaoImg: $('#platform-ipt1').attr('url'),
     mysiteImg: $('#platform-ipt2').attr('url'),
     myacountImg: $('#platform-ipt3').attr('url'),
