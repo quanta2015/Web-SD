9f4c1a9dbfc3d8cd36db0b446f2ccf7bd2ed3c59
quanta2006 <quanta2006@163.com>
2018-06-05 22:34:37 +0800

    fix

---
+++ sys/js/sell/listorder.js
@@ -0,0 +1,43 @@
+var _id;
+let pageData = Object.assign({}, PAGE_DATA);
+
+$(init);
+
+function init() {
+  _id = '201806021905000011'
+
+  initList(pageData);
+  // $('body').on('click', '.return-list', doReturnList);
+}
+
+function initList(param) {
+  Object.assign( param, { taskId: _id });
+  TmplData('/tmpl/sell/list_order.tmpl', [URL_SELL_ACCEPT_LIST, encodeQuery(param)].join('?'), null, cbListTask)
+}
+
+
+function cbListTask(r,e) {
+  let ret = e[0];
+  if (ret.code == 0) {
+    Object.assign(ret, pageData);
+    totalPages = Math.ceil(ret.total/PAGE_DATA.pageSize);
+    $(".portlet-body .table").remove();
+    $(".portlet-body").prepend($.templates(r[0]).render(ret, rdHelper));
+    if ($('.table-pg').text() == '') initPage(totalPages);
+  } else if (ret.code == 99) {
+    notifyInfo(ret.message);
+  } else if (ret.code == -1) {
+    relogin();
+  }
+
+}
+
+function initPage(totalPages) {
+  $('.portlet-body .table-pg').twbsPagination({
+    totalPages: totalPages || 1,
+    onPageClick: function(event, page) {
+      pageData.pageIndex = page - 1;
+      initList(pageData);
+    }
+  })
+}
