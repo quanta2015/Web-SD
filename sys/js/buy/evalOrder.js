e435c8b49e2c2505c3a303c67cad6c84b1aa08e0
yj441106 <yj441106@163.com>
2018-06-23 08:31:12 +0800

    评价说明信息

--- sys/js/buy/evalOrder.js
+++ sys/js/buy/evalOrder.js
@@ -12,6 +12,9 @@ function init() {
 
   $('body').on('click', '#submit-eval', doSubmitEval);
   $('body').on('click', '#return-list', gotoPage);
+  $('body').on('click','#watch-require',doEvalExplain);
+  $('body').on('click', '.m-close', doClose);
+  $('body').on('click','.b-close',doClose);
 }
 
 function gotoPage() {
@@ -46,7 +49,6 @@ function renderImg() {
   })
 }
 
-
 function doSubmitEval(e) {
 
   let err = false;
@@ -77,4 +79,17 @@ function cbSubmitEval(e) {
 
 function gotoPage() {
   location.href = 'listOrder.html'
+}
+
+function doEvalExplain(e){
+	var meshtm="<table width='100%' ><tr height='50px'><td align='center'>普通好评任务</td><td style='padding-left:20px'>评价要求：<br>好评，评价可自由发挥</td></tr>"+
+	"<tr height='80px'><td align='center'>指定关键词好评任务</td><td style='padding-left:20px'>评价要求：<br>文字好评内容须涵盖以下指定关键词<br>指定关键词：xxx</td></tr>"+
+	"<tr height='80px'><td align='center'>指定文字好评任务</td><td style='padding-left:20px'>评价要求：<br>请完全按照以下指定文字填写好评内容<br>指定文字：xxx</td></tr>"+
+	"<tr height='80px'><td align='center'>指定图片好评任务</td><td style='padding-left:20px'>评价要求：<br>请上传以下指定图片，并填写指定文字好评内容<br>图片：<input style='width:20px'/> <input style='width:20px'/><input style='width:20px'/><br>指定文字：xxx</td></tr>"+
+	"</table>"
+	bootbox.dialog({title:'评价说明',message:meshtm});
+}
+
+function doClose() {
+  $('.g-detail').hide()
 }
