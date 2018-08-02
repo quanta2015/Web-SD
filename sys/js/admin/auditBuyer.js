a155e443802ad3a9649e8fcca9f3fc5d99ca529a
宇宙 <2677267624@qq.com>
2018-08-02 14:09:41 +0800

    upddate buyer control

--- sys/js/admin/auditBuyer.js
+++ sys/js/admin/auditBuyer.js
@@ -24,6 +24,51 @@ function init() {
   $('body').on('click', '.btn-setvip', doRemarks);
 }
 
+function initList() {
+  let param = {
+    sdate: $("#sr-time-from").val() + ' 00:00:00',
+    edate: $("#sr-time-to").val() + ' 23:59:00',
+  };
+  Object.assign(param, pageData);
+  promiseTmpl('GET', '/tmpl/buy/list_buyer.tmpl', ['/admin/buyer_acount_list', encodeQuery(param)].join('?'), null, cbList)
+}
+
+function cbList(r, e) {
+  let ret = e;
+    _listshop = ret.data;
+    Object.assign(ret, pageData);
+    totalPages = Math.ceil(ret.total/PAGE_DATA.pageSize);
+    $(".portlet-body .table").remove();
+    $(".portlet-body .table-data").append($.templates(r).render(ret, rdHelper));
+    $(".fancybox").fancybox({'titlePosition':'inside','type':'image'});
+    if ($('.table-pg').text() == '') initPage(totalPages);
+}
+
+
+function initPage(totalPages) {
+  $('.portlet-body .table-pg').twbsPagination({
+    totalPages: totalPages || 1,
+    onPageClick: function(event, page) {
+      pageData.pageIndex = page - 1;
+      initList();
+    }
+  })
+}
+
+function initTime() {
+  let from =  moment().subtract('days',7).format('YYYY-MM-DD');
+  let to = moment().format('YYYY-MM-DD');
+  $("#sr-time-from").datetimepicker({ value: from, format:'Y-m-d', timepicker:false});
+  $("#sr-time-to").datetimepicker({value: to, format:'Y-m-d', timepicker:false});
+}
+
+function doSearch() {
+  $('.portlet-body .table-pg').remove();
+  $('.portlet-body').append('<div class="table-pg"></div>');
+  initList();
+}
+
+
 
 function doDetailTaoBao() {
   var obj ={
@@ -150,48 +195,4 @@ function doClose() {
 // }
 
 
-
-function initList() {
-  let param = {
-    sdate: $("#sr-time-from").val() + ' 00:00:00',
-    edate: $("#sr-time-to").val() + ' 23:59:00',
-  };
-  Object.assign(param, pageData);
-  promiseTmpl('GET', '/tmpl/buy/list_buyer.tmpl', ['/admin/shoper_acount_list', encodeQuery(param)].join('?'), null, cbList)
-}
-
-function cbList(r, e) {
-  let ret = e;
-    _listshop = ret.data;
-    Object.assign(ret, pageData);
-    totalPages = Math.ceil(ret.total/PAGE_DATA.pageSize);
-    $(".portlet-body .table").remove();
-    $(".portlet-body .table-data").append($.templates(r).render(ret, rdHelper));
-    $(".fancybox").fancybox({'titlePosition':'inside','type':'image'});
-    if ($('.table-pg').text() == '') initPage(totalPages);
-}
-
-
-function initPage(totalPages) {
-  $('.portlet-body .table-pg').twbsPagination({
-    totalPages: totalPages || 1,
-    onPageClick: function(event, page) {
-      pageData.pageIndex = page - 1;
-      initList();
-    }
-  })
-}
-
-function initTime() {
-  let from =  moment().subtract('days',7).format('YYYY-MM-DD');
-  let to = moment().format('YYYY-MM-DD');
-  $("#sr-time-from").datetimepicker({ value: from, format:'Y-m-d', timepicker:false});
-  $("#sr-time-to").datetimepicker({value: to, format:'Y-m-d', timepicker:false});
-}
-
-function doSearch() {
-  $('.portlet-body .table-pg').remove();
-  $('.portlet-body').append('<div class="table-pg"></div>');
-  initList();
-}
 
